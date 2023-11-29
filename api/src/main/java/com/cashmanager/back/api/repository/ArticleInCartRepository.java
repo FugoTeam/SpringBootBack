9b19dd596f0eeddf3a9cef5eca48eeeb7f583c18
Dulin <lucas.lou.b.dulin@accenture.com>
2023-11-29 12:29:33 +0100

    init

--- api/src/main/java/com/cashmanager/back/api/repository/ArticleInCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cashmanager.back.api.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cashmanager.back.api.model.ArticleInCart;
-
-public interface ArticleInCartRepository extends JpaRepository<ArticleInCart, Long> {
-
-}
