d0e1877f05982fc99205f84117710d0936b4f2ea
Camille TOURON <camille.touron.pro@gmail.com>
2024-04-09 15:09:38 +0200

    CT - Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-cd backend/ || exit 1
 
-echo "DATABASE_URL=${DATABASE_URL}" >> .env
-echo "DATABASE_USERNAME=${DATABASE_USERNAME}" >> .env
-echo "DATABASE_PASSWORD=${DATABASE_PASSWORD}" >> .env
-echo "PORT=${PORT}" >> .env
-
-if [ -z "$PORT" ]; then
-exit 1
+if [[ -z "$SERVER_USER" || -z "$SERVER_PWD" || -z "$SERVER_HOST" ]]; then
+    echo "Please set SERVER_USER, SERVER_PWD, and SERVER_HOST variables before running this script."
+    exit 1
 fi
 
-echo "Stopping process running on port $PORT..."
-sudo lsof -t -i:"$PORT" -sTCP:LISTEN | xargs -r kill
+REMOTE_COMMAND=$(cat <<EOF
+cd /home/ubuntu/project/SpringBootBack/backend || exit 1
 
 echo "Starting your application..."
 git checkout main
@@ -19,4 +14,10 @@ git pull
 rm -rf target
 mvn clean package -DskipTests
 docker build -t fugoworld_backend .
-docker run -p "${PORT}":"${PORT}" -e PORT="${PORT}" fugoworld_backend
+docker stop fugoworld_backend
+docker rm fugoworld_backend
+docker run -d -p "$PORT":"$PORT" -e PORT="$PORT" --name fugoworld_backend fugoworld_backend
+EOF
+)
+
+sshpass -p "$SERVER_PWD" ssh -o StrictHostKeyChecking=no "$SERVER_USER"@"$SERVER_HOST" "$REMOTE_COMMAND"
